188111d997fe533eaa8763ae0caa70e1ceef6cb9
昌维 <867597730@qq.com>
2016-11-09 21:19:59 +0800

    server.php翻译提交

--- server.php
+++ server.php
@@ -7,6 +7,12 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+/**
+ * translator:昌维 <867597730@qq.com>
+ * repository:https://github.com/cw1997/laravel-Simplified-Chinese
+ * Translated as 2016-11-09 21:19:06
+ */
+
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
@@ -14,6 +20,8 @@ $uri = urldecode(
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
+// 这个文件让我们在PHP内置的WEB服务器中模仿Apache的mod_rewrite(URL重写模块)功能
+// 这提供了一种方便的方法在没有安装真实WEB服务器软件的情况下来测试一个Larave应用
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
