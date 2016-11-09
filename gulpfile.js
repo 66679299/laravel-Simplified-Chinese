ad35715ad2c5d261e8f2478d37f043207a30f396
昌维 <867597730@qq.com>
2016-11-09 22:14:03 +0800

    gulpfile.js翻译提交

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,9 @@
+/**
+ * translator:昌维 <867597730@qq.com>
+ * repository:https://github.com/cw1997/laravel-Simplified-Chinese
+ * Translated as 2016-11-09 22:13:49
+ */
+
 const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue-2');
@@ -5,10 +11,14 @@ require('laravel-elixir-vue-2');
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
+ | Elixir资源管理器
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
  | for your Laravel application. By default, we are compiling the Sass
+ | file for your application as well as publishing vendor resources.
+ | Elixir为您的Laraval应用提供了一个干净流畅的API来定义一些简单的Gulp任务
+ | 通常情况下，我们将会为你的应用像第三方发布资源一样的编译Sass文件
  | file for your application as well as publishing vendor resources.
  |
  */
